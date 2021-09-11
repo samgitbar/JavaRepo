09d4e3cfff3ccf16e300e4c10ce79d6677ba4973
samgit5564 <samgit5564@gmail.com>
2021-09-12 00:18:07 +0300

    This commit is to re order the sys prints

--- example1/src/example1/main.java
+++ example1/src/example1/main.java
@@ -5,16 +5,15 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//This is better print order
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
 		System.out.println("Hello World3");
-
-		System.out.println("Hello World3");
-		
+		System.out.println("Hello World4");
 		System.out.println("Hello World5");
 
-		System.out.println("Hello World4");
+		
 
 
 	}
